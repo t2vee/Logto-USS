0dc27c2ebb4909b5a62bdbdce78b748d4cdf0e49
t2vee <68762600+t2vee@users.noreply.github.com>
2024-05-24 21:46:36 +1000

    cleaned up dangerzone handler

--- src/handlers/DangerZone.vue
+++ src/handlers/DangerZone.vue
@@ -1,24 +1,18 @@
 <script setup>
-import {AlertTriangle, OctagonPause} from "lucide-vue-next";
+import { defineAsyncComponent } from "vue";
+import { AlertTriangle, OctagonPause } from "lucide-vue-next";
 import { Accordion, AccordionContent, AccordionItem, AccordionTrigger } from '@/components/ui/accordion/index.js'
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from '@/components/ui/card/index.js'
-import DangerZoneAlertDialog from "@/components/Global/DangerZoneAlertDialog.vue";
-import TerminateAccount from "@/components/Pages/DangerZone/TerminateAccount.vue";
-import SuspendAccount from "@/components/Pages/DangerZone/SuspendAccount.vue";
+import { CardDescription, CardTitle } from '@/components/ui/card/index.js'
+const DangerZoneAlertDialog = defineAsyncComponent(() => import( "@/components/Global/DangerZoneAlertDialog.vue"));
+const TerminateAccount = defineAsyncComponent(() => import( "@/components/Pages/DangerZone/TerminateAccount.vue"));
+const SuspendAccount = defineAsyncComponent(() => import( "@/components/Pages/DangerZone/SuspendAccount.vue"));
 
 </script>
 
 <template>
   <div class="w-[600px] space-y-5">
     <CardTitle class="my-4 ">You are entering the Danger Zone</CardTitle>
-    <CardDescription class="">Here you can access destructive actions for managing your account. Most settings here can have potentially unwanted affects on your account. So be careful!</CardDescription>
+    <CardDescription>Here you can access destructive actions for managing your account. Most settings here can have potentially unwanted affects on your account. So be careful!</CardDescription>
     <Accordion type="single" collapsible>
       <AccordionItem value="item-1">
         <AccordionTrigger class="text-red-600">I Understand. Show me Account Actions...</AccordionTrigger>
