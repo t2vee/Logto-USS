4e37e6c82703432d19826fd5fbaab6226929e982
t2vee <68762600+t2vee@users.noreply.github.com>
2024-05-27 04:00:55 +1000

    removed old unused response and util files

--- src/responses/failedResponseWithMessage.js
+++
@@ -1,5 +0,0 @@
-// Copyright (c) 2024 t2vee. All rights reserved.
-// Use of this source code is governed by an MPL license. 
-
-
-export default (error) => Response.json(error, { status: error.status || 400, headers: { 'Content-Type': 'application/json' } })
--- src/utils/prepareNumber.js
+++ src/utils/prepareNumber.js
@@ -1,22 +1,12 @@
 // Copyright (c) 2024 t2vee. All rights reserved.
-// Use of this source code is governed by an MPL license. 
+// Use of this source code is governed by an MPL license.
 
-
-/**
- * Cleans and validates a user-provided phone number by removing spaces and the leading '+' sign.
- * It checks the cleaned number against a pattern to ensure it consists only of digits.
- * If the cleaned number fails the pattern test, it throws an error indicating an invalid phone number format.
- *
- * @param {string} userNumber The user-provided phone number to be cleaned and validated.
- * @returns {string} The cleaned phone number if it passes the validation.
- * @throws {Object} An error object with a message and status code if the phone number is invalid.
- */
 
 export default async function prepareNumber(userNumber) {
 	const phonePattern = /^\d+$/;
 	const cleanedNumber = userNumber.replaceAll(' ', '').replace('+', '');
 	if (!phonePattern.test(cleanedNumber)) {
-		throw { message: 'Invalid phone number format - ERR 3489', status: 400 };
+		throw { message: 'ERR_NUMBER_INVALID', status: 400 };
 	}
 	return cleanedNumber
 }
