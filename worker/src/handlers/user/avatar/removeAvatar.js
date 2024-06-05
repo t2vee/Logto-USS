4a595aa4bfffbf89c383799a489b47d28dcda408
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-06 08:35:50 +1000

    worker: migrated all user avatar routes onto new system

--- worker/src/handlers/user/avatar/removeAvatar.js
+++ worker/src/handlers/user/avatar/removeAvatar.js
@@ -4,15 +4,17 @@
 
 import successEMPTY from "../../../responses/raw/success-EMPTY";
 import failureCONTENT from "../../../responses/raw/failure-CONTENT";
+import { AvatarUserRouter } from './index'
 
-export default  async (request, env, ctx) => {
-	try {
-		await ctx.Http.patch(
-			`/api/users/${ctx.userid}`,
-			{data: {"avatar": null}
-			});
-		return successEMPTY(env)
-	} catch (e) {
-		console.error(e)
-		return failureCONTENT(env, e.message, e.status)	}
-}
+AvatarUserRouter.post("/remove", async (request, env, ctx) => {
+		try {
+			await ctx.Http.patch(
+				`/api/users/${ctx.userid}`,
+				{data: {"avatar": null}
+				});
+			return successEMPTY(env)
+		} catch (e) {
+			console.error(e)
+			return failureCONTENT(env, e.message, e.status)	}
+	}
+)
