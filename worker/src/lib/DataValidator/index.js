296569f7c8600d19a979b31e4f9a7ad525b5df1a
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-06 05:51:09 +1000

    worker: removed all older validators and put the validation logic in the DataValidator class

--- worker/src/lib/DataValidator/index.js
+++ worker/src/lib/DataValidator/index.js
@@ -1,34 +1,77 @@
 // Copyright (c) 2024 t2vee. All rights reserved.
 // Use of this source code is governed by an MPL license.
 
-import * as validator from 'validators'
 import Filter from "bad-words";
 import { ValidationException } from '../../exceptions/ValidationException'
+import * as schema from './schemas';
 
-export default class DataValidator {
+
+import { assert } from 'superstruct'
+
+class DataValidator {
 	constructor(env) {
 		this._env = env;
 		this._filter = new Filter();
 	}
 
+	#validate(data, schema, err, checkBadWords = false) {
+		try {
+			assert(data, schema, err)
+		} catch (e) {
+			throw new ValidationException(e.Ge);
+		}
+		if (checkBadWords) {
+			console.log('im checking for naughty words')
+			if (this._filter.isProfane(data.name ? data.name : data.username)) {{throw new ValidationException('ERR_CONTAINS_BAD_WORDS', 406)}}
+		}
+	}
+
+	#calculateAge(data) {
+		const ageDifMs = Date.now() - data.getTime();
+		const ageDate = new Date(ageDifMs);
+		return Math.abs(ageDate.getUTCFullYear() - 1970);
+	}
+
+	#verifyDate(data) {
+		const dateParts = data.birthday.split(' ');
+		const date = new Date(`${dateParts[1]} ${dateParts[0]}, ${dateParts[2]}`);
+		if (!isNaN(date.getTime())) {
+			const age = this.#calculateAge(date);
+			console.log(age)
+			if (age <= 13 || age >= 99) {throw new ValidationException('ERR_OUT_OF_RANGE', 422)}
+		} else {
+			throw new ValidationException('ERR_INVALID_DATE', 422);
+		}
+	}
+
 	username(data) {
-		if (validator.validateUsername(data)) {throw new ValidationException()}
-		if (this._filter.isProfane(data)) {{throw new ValidationException('ERR_CONTAINS_BAD_WORDS')}}
+		return this.#validate(data, schema.UsernameSchema, 'ERR_INVALID_USERNAME', true)
 	}
+
 	fullName(data) {
-		if (validator.validateName(data)) {throw new ValidationException()}
-		if (this._filter.isProfane(data)) {{throw new ValidationException('ERR_CONTAINS_BAD_WORDS')}}
+		return this.#validate(data, schema.NameSchema, 'ERR_INVALID_NAME', true)
 	}
+
 	password(data) {
-		if (validator.validatePassword(data)) {throw new ValidationException()}
+		return this.#validate(data, schema.PasswordSchema, 'ERR_INVALID_PASSWORD')
 	}
+
 	email(data) {
-		if (validator.validateEmail(data)) {throw new ValidationException()}
+		return this.#validate(data, schema.EmailSchema, 'ERR_INVALID_EMAIL')
 	}
 	phone(data) {
-		if (validator.validatePhone(data)) {throw new ValidationException()}
+		return this.#validate(data, schema.PhoneSchema, 'ERR_INVALID_NUMBER')
 	}
 	birthday(data) {
-		if (validator.validateBirthday(data)) {throw new ValidationException()}
+		try {
+			this.#validate(data, schema.BirthdaySchema, 'ERR_INVALID_DATE');
+			this.#verifyDate(data);
+		} catch (e) {
+			throw e;
+		}
 	}
+}
+
+export function createDataValidator(env) {
+	return new DataValidator(env);
 }
