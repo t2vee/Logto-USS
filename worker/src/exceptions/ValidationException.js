70ba5872fe6189465bb33519c73b4ea60502b258
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-06 05:52:29 +1000

    worker: changed the default status code of the validation exception from 400 to 422

--- worker/src/exceptions/ValidationException.js
+++ worker/src/exceptions/ValidationException.js
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an MPL license.
 
 export class ValidationException extends Error {
-	constructor(message = 'ERR_FAILED_VALIDATION', status = 400) {
+	constructor(message = 'ERR_FAILED_VALIDATION', status = 422) {
 		super(message);
 		this.status = status;
 	}
