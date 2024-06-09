867ef32c2e33c6137d64d3bb6775097401196f3c
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-10 08:20:51 +1000

    worker: implement route and route logic

--- worker/src/handlers/user/update/index.js
+++ worker/src/handlers/user/update/index.js
@@ -8,6 +8,7 @@ import { handler as UpdateFullName } from "./updateFullName";
 import { handler as UpdatePassword } from "./updatePassword";
 import { handler as UpdateRegionalSettings } from "./updateRegionalSettings";
 import { handler as UpdateUsername } from "./updateUsername";
+import { handler as UpdateDeveloperStatus } from "./updateDeveloperStatus";
 import removeSMS from "../mfa/new/remove-number";
 
 
@@ -20,4 +21,5 @@ UpdateUserRouter
 	.post("/password", UpdatePassword)
 	.post("/regional-settings", UpdateRegionalSettings)
 	.post("/username", UpdateUsername)
-  .post("/remove-sms",removeSMS)
+  .post("/remove-sms", removeSMS)
+	.post("/developer-status", UpdateDeveloperStatus)
