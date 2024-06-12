3bcba935ad0941438623ff679d5c5813538fc4d0
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-12 21:29:15 +1000

    web: converted edit username dialog

--- web/src/components/Pages/PersonalInfo/EditUsername.vue
+++ web/src/components/Pages/PersonalInfo/EditUsername.vue
@@ -5,12 +5,13 @@ import axios from 'redaxios'
 import { inject, ref, onMounted } from 'vue'
 import { useLogto } from '@logto/vue'
 import debounce from 'lodash/debounce'
-import { Ban, UserRoundCheck, MoreHorizontal } from 'lucide-vue-next'
+import {Ban, UserRoundCheck, MoreHorizontal, CircleUserRound} from 'lucide-vue-next'
 import { Button } from '@/components/ui/button/index.js'
 import { DialogClose, DialogFooter } from '@/components/ui/dialog/index.js'
 import { toast } from 'vue-sonner'
 import { eventBus } from '@/lib/eventBus.js'
 import ConnectorAlert from '@/components/Global/ConnectorAlert.vue'
+import MfaVerificationDialog from "@/components/Global/MFAHelpers/MfaVerificationDialog.vue";
 
 const userData = inject('userData')
 
@@ -126,68 +127,66 @@ onMounted(checkNextUsernameChange)
 </script>
 
 <template>
-  <div class="space-y-10">
-    <ConnectorAlert
-      v-if="waitForNextChange"
-      :custom-title="`Cant Change Username Until ${waitForNextChange.value}`"
-      :custom-message="`You can only change your username once per month. Your next username change will be available on the ${waitForNextChange.value}`"
-    />
-    <div class="flex flex-col gap-4 pb-4 items-center align-middle">
-      <div class="grid w-3/4 max-w-sm items-center gap-1.5 relative">
-        <Label for="username" class="flex font-bold w-full justify-between">
-          Username
-          <span v-if="badWords" class="text-xs text-red-500">Contains Bad Words</span>
-          <span v-else-if="isAvailable && usernameChecked" class="text-xs text-green-500"
-            >Username Available!</span
-          >
-          <span v-else-if="!isAvailable && usernameChecked" class="text-red-500"
-            >Username Taken</span
-          >
-        </Label>
-        <div>
-          <Input
-            id="username"
-            :disabled="!!waitForNextChange"
-            v-model="username"
-            @input="handleInput"
-            :class="{
+  <MfaVerificationDialog title="Username" :icon="CircleUserRound" :desc="userData.username ?? userData.name ?? 'Not Set'" >
+    <template #body>
+      <ConnectorAlert
+          v-if="waitForNextChange"
+          :custom-title="`Cant Change Username Until ${waitForNextChange.value}`"
+          :custom-message="`You can only change your username once per month. Your next username change will be available on the ${waitForNextChange.value}`"
+      />
+      <div class="w-full h-full flex flex-col gap-4 pb-4 items-center align-middle mt-3">
+        <div class="grid w-3/5 max-w-sm items-center gap-1.5 relative">
+          <Label for="username" class="flex font-bold w-full justify-between">
+            Username
+            <span v-if="badWords" class="text-xs text-red-500">Contains Bad Words</span>
+            <span v-else-if="isAvailable && usernameChecked" class="text-xs text-green-500">
+              Username Available!
+            </span>
+            <span v-else-if="!isAvailable && usernameChecked" class="text-red-500">
+              Username Taken
+            </span>
+          </Label>
+          <div>
+            <Input
+                id="username"
+                :disabled="!!waitForNextChange"
+                v-model="username"
+                @input="handleInput"
+                :class="{
               'border-red-500': !isAvailable && usernameChecked,
               'border-green-500': isAvailable && usernameChecked
             }"
-            :placeholder="userData.username ? userData.username : userData.name"
-          />
-          <div class="absolute inset-y-0 right-0 flex items-center pt-7 pr-1">
-            <MoreHorizontal v-if="isChecking" />
-            <UserRoundCheck v-else-if="isAvailable && usernameChecked" class="text-green-500" />
-            <Ban v-else-if="!isAvailable && usernameChecked" class="text-red-500" />
+                :placeholder="userData.username ? userData.username : userData.name"
+            />
+            <div class="absolute inset-y-0 right-0 flex items-center pt-7 pr-1">
+              <MoreHorizontal v-if="isChecking" />
+              <UserRoundCheck v-else-if="isAvailable && usernameChecked" class="text-green-500" />
+              <Ban v-else-if="!isAvailable && usernameChecked" class="text-red-500" />
+            </div>
           </div>
         </div>
+        <p class="text-xs" v-if="!waitForNextChange">
+          Keep in mind, You can only <strong>change your username once per month!</strong>
+        </p>
       </div>
-      <p class="text-xs" v-if="!waitForNextChange">
-        Keep in mind, You can only <strong>change your username once per month!</strong>
-      </p>
-    </div>
-    <DialogFooter>
-      <div class="flex space-x-10 items-center align-middle">
-        <Button variant="link" as-child>
-          <a target="_blank" href="/legal"> Privacy and Cookies Policy </a>
-        </Button>
-        <div class="space-x-2">
-          <Button
-            type="submit"
-            class="h-[30px]"
-            :disabled="waitForNextChange || !isAvailable"
-            @click="updateData"
-          >
-            Save
-          </Button>
-          <DialogClose as-child>
-            <Button type="button" variant="outline" class="h-[30px]"> Cancel </Button>
-          </DialogClose>
-        </div>
-      </div>
-    </DialogFooter>
-  </div>
+    </template>
+    <template #footer>
+      <DialogClose as-child>
+        <Button type="button" variant="outline" class="h-[30px]"> Cancel </Button>
+      </DialogClose>
+      <Button variant="link" as-child size="xs">
+        <a target="_blank" href="/legal" class="text-sm"> Privacy and Cookies Policy </a>
+      </Button>
+      <Button
+          type="submit"
+          class="h-[30px]"
+          :disabled="waitForNextChange || !isAvailable"
+          @click="updateData"
+      >
+        Save
+      </Button>
+    </template>
+  </MfaVerificationDialog>
 </template>
 
 <style scoped></style>
