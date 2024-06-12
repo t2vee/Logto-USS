c4d208d39434ded7a7989291efca3699d192abc1
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-12 23:50:42 +1000

    web: converted mfa methods dialog

--- web/src/components/Pages/SignInAndSecurity/EditMfaMethods.vue
+++ web/src/components/Pages/SignInAndSecurity/EditMfaMethods.vue
@@ -6,10 +6,9 @@ import {
   TabletSmartphone,
   Check,
   MailCheck,
-  MailX
+  MailX, Fingerprint
 } from 'lucide-vue-next'
-import { inject, ref } from 'vue'
-import { DialogFooter } from '@/components/ui/dialog/index.js'
+import {inject, onMounted, ref} from 'vue'
 import { Button } from '@/components/ui/button/index.js'
 import { PhoneMissed, DoorOpen } from 'lucide-vue-next'
 
@@ -17,13 +16,13 @@ import ConnectorAlert from '@/components/Global/ConnectorAlert.vue'
 import EditAppAuthenticator from '@/components/Pages/SignInAndSecurity/EditAppAuthenticator.vue'
 import AddPhoneNumberDialog from '@/components/Pages/SignInAndSecurity/AddPhoneNumberDialog.vue'
 import EditPhoneNumberDialog from '@/components/Pages/SignInAndSecurity/RemovePhoneNumberDialog.vue'
-import EditDetailDialog from '@/components/Global/MFAHelpers/EditDetailDialog.vue'
 import EditEmailAddress from '@/components/Pages/SignInAndSecurity/EditEmailAddress.vue'
+import MfaVerificationDialog from "@/components/Global/MFAHelpers/MfaVerificationDialog.vue";
 
 const userData = inject('userData')
-const mfaMethods = inject('mfaMethods')
+const mfaOptions = inject('mfaMethods')
 
-// i hate this but im also lazy so...
+// i hate this but im also lazy so and also lucide makes it impossible to dynamically color icons. like bruh let me use tailwind
 const emailMouseOver = ref(false)
 const phoneMouseOver = ref(false)
 const appMouseOver = ref(false)
@@ -42,141 +41,140 @@ const resetDefault = () => {
   phoneMouseOver.value = false
   appMouseOver.value = false
 }
+
+const mfaOptionsNum = ref([])
+
+async function parseMfaOptions() {
+  mfaOptionsNum.value.push(userData.value.email)
+  if (userData.value.phone_number) {mfaOptionsNum.value.push(userData.value.phone_number)}
+  if (mfaOptions.value[0] !== 'none') {mfaOptionsNum.value.push(mfaOptions.value[0])}
+}
+
+onMounted(parseMfaOptions)
 </script>
 
 <template>
-  <Transition name="fade" mode="out-in">
-    <div v-if="emailActive" class="space-y-3">
-      <ConnectorAlert
-        custom-title="You Cannot Disable Email as a MFA Method"
-        custom-message="Every account must have a valid email address connected and as such must always be available as a MFA method. You may edit your email address below:"
-      />
-      <EditDetailDialog
-        title="Email Address"
-        :desc="
-          userData.email.length > 30
-            ? userData.email.substring(0, 30) + '...'
-            : userData.email
-        "
-        :icon="userData.email_verified ? MailCheck : MailX"
-        :dialog-page="EditEmailAddress"
-      />
-    </div>
-    <div v-else-if="phoneActive" class="space-y-3 flex flex-col items-center">
-      <p class="text-sm w-2/3 text-center">
-        When a phone number is added to your account it is used <strong>only</strong> for MFA. To
-        {{ userData.phone_number ? 'disable' : 'enable' }} MFA via SMS,
-        {{ userData.phone_number ? 'remove' : 'add' }} your number below:
-      </p>
-      <div class="w-2/3">
-        <EditDetailDialog
-          title="Phone Number"
-          :desc="userData.phone_number ? userData.phone_number : 'Number Not Added'"
-          :icon="userData.phone_number_verified ? Phone : PhoneMissed"
-          :dialog-page="userData.phone_number ? EditPhoneNumberDialog : AddPhoneNumberDialog"
-        />
-      </div>
-    </div>
-    <div v-else-if="appActive">
-      <EditAppAuthenticator v-model="removeFooter" :mfa-methods="mfaMethods" />
-    </div>
-    <div v-else class="flex flex-col items-center max-w-[350px] mt-[-30px] gap-y-3">
-      <p class="text-sm text-center">
-        Add or remove Multi-Factor Authentication methods from your account. Select one below to
-        continue
-      </p>
-      <div class="grid grid-cols-2 gap-4 w-full">
-        <div>
-          <Label
-            for="email"
-            class="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-[#030607] hover:text-secondary peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary hover:cursor-pointer"
-            @mouseenter="() => (emailMouseOver = true)"
-            @mouseleave="() => (emailMouseOver = false)"
-            @click="() => (emailActive = true)"
-          >
-            <Mails
-              :size="32"
-              :color="emailMouseOver? 'rgba(161 85% 86%)': userData.email_verified? 'rgb(34 197 94)': ''"
-              class="m-2"
-            />
-            <div class="flex items-center justify-between" :class="emailMouseOver ? 'text-secondary' : userData.email_verified ? 'text-green-500' : ''">
-              <Check
-                v-if="userData.email_verified"
-                :size="16"
-                :color="emailMouseOver? 'rgba(161 85% 86%)': userData.email_verified? 'rgb(34 197 94)': ''"
-              />
-              Email Address
-            </div>
-          </Label>
+  <MfaVerificationDialog title="Account Security" :icon="Fingerprint" :desc="`${mfaOptionsNum.length} MFA Method${mfaOptionsNum.length === 1 ? '' : 's'} Set Up`">
+    <template #body>
+      <Transition name="fade" mode="out-in" duration="100">
+        <div v-if="emailActive" class="space-y-3 mb-10">
+          <ConnectorAlert
+              custom-title="You Cannot Disable Email as a MFA Method"
+              custom-message="Every account must have a valid email address connected and as such must always be available as a MFA method. You may edit your email address below:"
+          />
+          <EditEmailAddress />
         </div>
-        <div>
-          <Label
-            for="phone"
-            class="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-[#030607] hover:text-secondary peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary hover:cursor-pointer"
-            @mouseenter="() => (phoneMouseOver = true)"
-            @mouseleave="() => (phoneMouseOver = false)"
-            @click="() => (phoneActive = true)"
-          >
-            <Phone
-              :size="32"
-              :color="phoneMouseOver? 'rgba(161 85% 86%)': userData.phone_number_verified? 'rgb(34 197 94)': ''"
-              class="m-2"
-            />
-            <div
-              class="flex items-center justify-between"
-              :class="phoneMouseOver? 'text-secondary': userData.phone_number_verified? 'text-green-500': ''"
-            >
-              <Check
-                v-if="userData.phone_number_verified"
-                :size="16"
-                :color="phoneMouseOver? 'rgba(161 85% 86%)': userData.phone_number_verified? 'rgb(34 197 94)': ''"
-              />
-              Phone Number
-            </div>
-          </Label>
-        </div>
-      </div>
-      <div class="w-full">
-        <div>
-          <Label
-              for="app"
-              class="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-[#030607] hover:text-secondary peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary hover:cursor-pointer"
-              @mouseenter="() => (appMouseOver = true)"
-              @mouseleave="() => (appMouseOver = false)"
-              @click="() => (appActive = true)"
-          >
-            <TabletSmartphone
-                :size="32"
-                :color="appMouseOver? 'rgba(161 85% 86%)': mfaMethods.totp? 'rgb(34 197 94)': ''"
-                class="m-2"
-            />
-            <div
-                class="flex items-center justify-between"
-                :class="appMouseOver? 'text-secondary': mfaMethods.totp? 'text-green-500': ''"
-            >
-              <Check
-                  v-if="mfaMethods.totp"
-                  :size="16"
-                  :color="appMouseOver? 'rgba(161 85% 86%)': mfaMethods.totp? 'rgb(34 197 94)': ''"
-              />
-              App Authenticator
-            </div>
-          </Label>
-        </div>
-      </div>
-    </div>
-  </Transition>
-  <Transition name="fade" mode="out-in">
-    <DialogFooter>
-      <div v-if="!removeFooter && (emailActive || phoneActive || appActive)" class="mt-6 w-full">
-          <div class="flex justify-center items-center align-middle <!--gap-x-20-->">
-            <Button variant="secondary" class="h-[30px]" @click="resetDefault">
-              <DoorOpen :stroke-wdth="1.5" color="black" class="pr-1" />
-              Go Back
-            </Button>
+        <div v-else-if="phoneActive" class="space-y-3 flex flex-col items-center mb-10">
+          <p class="text-sm w-2/3 text-center">
+            When a phone number is added to your account it is used <strong>only</strong> for MFA. To
+            {{ userData.phone_number ? 'disable' : 'enable' }} MFA via SMS,
+            {{ userData.phone_number ? 'remove' : 'add' }} your number below:
+          </p>
+          <div class="w-2/3">
+            <component :is="userData.phone_number ? EditPhoneNumberDialog : AddPhoneNumberDialog" />
           </div>
-      </div>
-      <div v-else-if="!emailActive || !phoneActive || !appActive"></div>
-    </DialogFooter>
-  </Transition>
+        </div>
+        <div v-else-if="appActive" class="mb-10">
+          <EditAppAuthenticator v-model="removeFooter" :mfa-methods="mfaOptions" />
+        </div>
+        <div v-else class="flex flex-col items-center max-w-[350px] mt-[-30px] gap-y-3">
+          <p class="text-sm text-center">
+            Add or remove Multi-Factor Authentication methods from your account. Select one below to
+            continue
+          </p>
+          <div class="grid grid-cols-2 gap-4 w-full">
+            <div>
+              <Label
+                  for="email"
+                  class="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-[#030607] hover:text-secondary peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary hover:cursor-pointer"
+                  @mouseenter="() => (emailMouseOver = true)"
+                  @mouseleave="() => (emailMouseOver = false)"
+                  @click="() => (emailActive = true)"
+              >
+                <Mails
+                    :size="32"
+                    :color="emailMouseOver? 'rgba(161 85% 86%)': userData.email_verified? 'rgb(34 197 94)': ''"
+                    class="m-2"
+                />
+                <div class="flex items-center justify-between" :class="emailMouseOver ? 'text-secondary' : userData.email_verified ? 'text-green-500' : ''">
+                  <Check
+                      v-if="userData.email_verified"
+                      :size="16"
+                      :color="emailMouseOver? 'rgba(161 85% 86%)': userData.email_verified? 'rgb(34 197 94)': ''"
+                  />
+                  Email Address
+                </div>
+              </Label>
+            </div>
+            <div>
+              <Label
+                  for="phone"
+                  class="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-[#030607] hover:text-secondary peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary hover:cursor-pointer"
+                  @mouseenter="() => (phoneMouseOver = true)"
+                  @mouseleave="() => (phoneMouseOver = false)"
+                  @click="() => (phoneActive = true)"
+              >
+                <Phone
+                    :size="32"
+                    :color="phoneMouseOver? 'rgba(161 85% 86%)': userData.phone_number_verified? 'rgb(34 197 94)': ''"
+                    class="m-2"
+                />
+                <div
+                    class="flex items-center justify-between"
+                    :class="phoneMouseOver? 'text-secondary': userData.phone_number_verified? 'text-green-500': ''"
+                >
+                  <Check
+                      v-if="userData.phone_number_verified"
+                      :size="16"
+                      :color="phoneMouseOver? 'rgba(161 85% 86%)': userData.phone_number_verified? 'rgb(34 197 94)': ''"
+                  />
+                  Phone Number
+                </div>
+              </Label>
+            </div>
+          </div>
+          <div class="w-full">
+            <div>
+              <Label
+                  for="app"
+                  class="flex flex-col items-center justify-between rounded-md border-2 border-muted bg-popover p-4 hover:bg-[#030607] hover:text-secondary peer-data-[state=checked]:border-primary [&:has([data-state=checked])]:border-primary hover:cursor-pointer"
+                  @mouseenter="() => (appMouseOver = true)"
+                  @mouseleave="() => (appMouseOver = false)"
+                  @click="() => (appActive = true)"
+              >
+                <TabletSmartphone
+                    :size="32"
+                    :color="appMouseOver? 'rgba(161 85% 86%)': mfaOptions.totp? 'rgb(34 197 94)': ''"
+                    class="m-2"
+                />
+                <div
+                    class="flex items-center justify-between"
+                    :class="appMouseOver? 'text-secondary': mfaOptions.totp? 'text-green-500': ''"
+                >
+                  <Check
+                      v-if="mfaOptions.totp"
+                      :size="16"
+                      :color="appMouseOver? 'rgba(161 85% 86%)': mfaOptions.totp? 'rgb(34 197 94)': ''"
+                  />
+                  App Authenticator
+                </div>
+              </Label>
+            </div>
+          </div>
+        </div>
+      </Transition>
+    </template>
+    <template #footer>
+      <div></div>
+      <Transition name="fade" mode="out-in" duration="100">
+        <div v-if="!removeFooter && (emailActive || phoneActive || appActive)">
+          <Button variant="secondary" class="h-[30px]" @click="resetDefault">
+            <DoorOpen :stroke-wdth="1.5" color="black" class="pr-1" />
+            Go Back
+          </Button>
+        </div>
+        <div v-else-if="!emailActive || !phoneActive || !appActive"></div>
+      </Transition>
+    </template>
+  </MfaVerificationDialog>
 </template>
--- web/src/handlers/SignIn&Security.vue
+++ web/src/handlers/SignIn&Security.vue
@@ -9,69 +9,31 @@ import EditDetailDialog from '@/components/Global/MFAHelpers/EditDetailDialog.vu
 import EditEmailAddress from '@/components/Pages/SignInAndSecurity/EditEmailAddress.vue'
 import EditMfaMethods from '@/components/Pages/SignInAndSecurity/EditMfaMethods.vue'
 import UpdatePasswordDialog from '@/components/Pages/SignInAndSecurity/UpdatePasswordDialog.vue'
+import MfaVerificationDialog from "@/components/Global/MFAHelpers/MfaVerificationDialog.vue";
 
 const userData = inject('userData')
 const mfaOptions = inject('mfaMethods')
-const mfaOptionsNum = ref([])
-
-async function parseMfaOptions() {
-  mfaOptionsNum.value.push(userData.value.email)
-  if (userData.value.phone_number) {mfaOptionsNum.value.push(userData.value.phone_number)}
-  if (mfaOptions.value[0] !== 'none') {mfaOptionsNum.value.push(mfaOptions.value[0])}
-}
 
 provide('mfaMethods', mfaOptions)
-
-onMounted(parseMfaOptions)
 </script>
 
 <template>
   <div class="w-[600px]">
     <CardTitle class="my-4">Sign-In and Security</CardTitle>
-    <CardDescription
-      >Manage settings related to signing in to your account, account security, as well as how to
-      recover your data when you're having trouble signing in.</CardDescription
-    >
+    <CardDescription>
+      Manage settings related to signing in to your account, account security, as well as how to
+      recover your data when you're having trouble signing in.
+    </CardDescription>
     <div class="flex gap-4 mt-12">
-      <EditDetailDialog
-        title="Email Address"
-        :desc="
-          userData.email.length > 30
-            ? userData.email.substring(0, 30) + '...'
-            : userData.email
-        "
-        :icon="userData.email_verified ? MailCheck : MailX"
-        :dialog-page="EditEmailAddress"
-      />
-      <EditDetailDialog
-        title="Phone Number"
-        :desc="userData.phone_number ? userData.phone_number : 'Number Not Added'"
-        :icon="userData.phone_number_verified ? Phone : PhoneMissed"
-        :dialog-page="userData.phone_number ? EditPhoneNumberDialog : AddPhoneNumberDialog"
-      />
+      <EditEmailAddress />
+      <component :is="userData.phone_number ? EditPhoneNumberDialog : AddPhoneNumberDialog" />
     </div>
     <div class="flex gap-4 mt-4">
-      <EditDetailDialog
-        title="Password"
-        :desc="`Last Logged in at ${new Date(userData.lastSignInAt).toLocaleDateString()}`"
-        :icon="CircleEllipsis"
-        :dialog-page="UpdatePasswordDialog"
-      />
-      <EditDetailDialog
-        title="Account Security"
-        :desc="`${mfaOptionsNum.length} MFA Method${mfaOptionsNum.length === 1 ? '' : 's'} Set Up`"
-        :icon="Fingerprint"
-        :dialog-page="EditMfaMethods"
-      />
+      <UpdatePasswordDialog />
+      <EditMfaMethods />
     </div>
     <div class="flex gap-4 mt-4">
-      <EditDetailDialog
-        disabled
-        title="Recovery Steps"
-        desc="Not Avaliable"
-        :icon="KeyRound"
-        :dialog-page="EditEmailAddress"
-      />
+      <MfaVerificationDialog disabled title="Recovery Steps" icon="KeyRound" desc="Not Available" />
     </div>
   </div>
 </template>
