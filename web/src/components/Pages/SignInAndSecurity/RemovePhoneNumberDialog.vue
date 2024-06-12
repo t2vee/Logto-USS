cec0ba0cf078df488b76a2b15661ec1ef16dd180
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-12 23:48:23 +1000

    web: converted phone number dialogs

--- web/src/components/Pages/SignInAndSecurity/RemovePhoneNumberDialog.vue
+++ web/src/components/Pages/SignInAndSecurity/RemovePhoneNumberDialog.vue
@@ -2,11 +2,12 @@
 import { ref, inject } from 'vue'
 import { useLogto } from '@logto/vue'
 import { DialogClose, DialogFooter } from '@/components/ui/dialog/index.js'
-import { Loader2 } from 'lucide-vue-next'
+import {Loader2, Phone} from 'lucide-vue-next'
 import { Button } from '@/components/ui/button/index.js'
 import axios from 'redaxios'
 import { toast } from 'vue-sonner'
 import { eventBus } from '@/lib/eventBus.js'
+import MfaVerificationDialog from "@/components/Global/MFAHelpers/MfaVerificationDialog.vue";
 
 const { getAccessToken } = useLogto()
 const isLoading = ref(false)
@@ -30,49 +31,48 @@ async function removeNumber() {
       toast.success('Number Successfully Removed', {
         description: 'You can now add a new number to your account.'
       })
+      isLoading.value = false
+      eventBus.emit('closeEditDetailDialog', false)
+      eventBus.emit('refreshUserData', true)
     }
   } catch (error) {
     toast.error('Error Removing Number:', { description: 'Service Unavailable. Try again later' })
-  } finally {
-    isLoading.value = false
-    eventBus.emit('closeEditDetailDialog', false)
-    eventBus.emit('refreshUserData', true)
   }
 }
 </script>
 
 <template>
-  <div class="flex flex-col items-center align-middle justify-center">
-    <div class="mb-8 w-full">
-      <p class="font-bold text-sm">
-        You can only have one number per account. To change your connected number, remove it first
-        then add a number as normal.
-      </p>
-      <br />
-      <div class="flex justify-evenly">
-        <p>Currently Connected Number:</p>
-        <strong>{{ userData.phone_number }}</strong>
-      </div>
-    </div>
-    <DialogFooter>
-      <div class="flex space-x-7 items-center align-middle">
-        <div class="flex space-x-2">
-          <Button
-            @click="removeNumber"
-            variant="destructive"
-            class="h-[30px]"
-            :disabled="isLoading"
-          >
-            <Loader2 v-if="isLoading" class="w-4 h-4 mr-2 animate-spin" />
-            Remove
-          </Button>
-          <DialogClose as-child>
-            <Button type="button" variant="outline" class="h-[30px]"> Cancel </Button>
-          </DialogClose>
+  <MfaVerificationDialog title="Remove Phone Number" :icon="Phone" :desc="userData.phone_number" edit>
+    <template #body>
+      <div class="flex flex-col items-center align-middle justify-center">
+        <div class="mb-8 w-full">
+          <p class="font-bold text-sm">
+            You can only have one number per account. To change your connected number, remove it first
+            then add a number as normal.
+          </p>
+          <br />
+          <div class="flex justify-evenly">
+            <p>Currently Connected Number:</p>
+            <strong>{{ userData.phone_number }}</strong>
+          </div>
         </div>
       </div>
-    </DialogFooter>
-  </div>
+    </template>
+    <template #footer>
+      <DialogClose as-child>
+        <Button type="button" variant="outline" class="h-[30px]"> Cancel </Button>
+      </DialogClose>
+      <Button
+          @click="removeNumber"
+          variant="destructive"
+          class="h-[30px]"
+          :disabled="isLoading"
+      >
+        <Loader2 v-if="isLoading" class="w-4 h-4 mr-2 animate-spin" />
+        Remove
+      </Button>
+    </template>
+  </MfaVerificationDialog>
 </template>
 
 <style scoped></style>
