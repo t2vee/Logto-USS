20c83a3588966ac52589ee767ed0efd43150a61b
t2vee <68762600+t2vee@users.noreply.github.com>
2024-06-17 15:42:00 +1000

    worker: started on data history lib

--- worker/src/lib/DataHistory/index.js
+++ worker/src/lib/DataHistory/index.js
@@ -1,3 +1,30 @@
 // Copyright (c) 2024 t2vee. All rights reserved.
 // Use of this source code is governed by an MPL license.
 
+import { ValidationException } from '../../exceptions/ValidationException'
+import * as schema from './schemas';
+
+
+import { assert } from 'superstruct'
+
+class DataHistory {
+	constructor(env) {
+		this._env = env;
+	}
+
+	#log(data, schema, err, checkBadWords = false) {
+		try {
+			assert(data, schema, err)
+		} catch (e) {
+			throw new ValidationException(e.Ge);
+		}
+		if (checkBadWords) {
+			if (this._filter.isProfane(data.name ? data.name : data.username)) {{throw new ValidationException('ERR_CONTAINS_BAD_WORDS', 406)}}
+		}
+		console.log('[VALIDATOR] Submitted data successfully validated')
+	}
+}
+
+export function createDataHistory(env) {
+	return new DataHistory(env);
+}
