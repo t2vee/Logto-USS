99ed147e9f20fc6246d155b393f31979bf9688a8
t2vee <68762600+t2vee@users.noreply.github.com>
2024-07-07 14:13:41 +1000

    refactor: removed root functions file

--- functions/index.js
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2024 t2vee. All rights reserved.
-// Use of this source code is governed by an MPL license.
-
-export async function onRequest(context) {
-    return new Response("Hello, World!")
-}
